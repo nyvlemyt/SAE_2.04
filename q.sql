3a79579c3e69da2ef27e0f94c0eb13375d8e68df
Johann <johannzidee2004@gmail.com>
2023-05-23 16:52:47 +0200

    q.sql maj

--- q.sql
+++ q.sql
@@ -3,4 +3,4 @@ AS
 SELECT p.nom, p.prenom, m.id_matiere, avg(note) as moyenne
 FROM etudiant e, matiere m, controle c, notes n, personne p
 WHERE m.id_matiere=c.id_matiere AND p.id_personne=e.id_personne AND c.id_controle=n.id_controle AND n.id_personne=e.id_personne
-GROUP BY p.nom, p.prenom, m.id_matiere, note;
+GROUP BY p.nom, p.prenom, m.id_matiere;
